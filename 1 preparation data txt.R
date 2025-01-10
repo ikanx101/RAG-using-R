f3d4a42f3c16dafc74fccef15eb8f3223fb57d25
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2025-01-10 15:46:07 +0700

    Update 1 preparation data txt.R

--- 1 preparation data txt.R
+++ 1 preparation data txt.R
@@ -1,3 +1,4 @@
+# Kode 1
 rm(list=ls())
 gc()
 
@@ -10,43 +11,22 @@ library(parallel)
 library(tidytext)
 ncore = detectCores()
 
-# panggil semua stop words
-stop = readLines("https://raw.githubusercontent.com/stopwords-iso/stopwords-id/master/stopwords-id.txt")
-
-# kita bikin function untuk mengambil datanya
-ambilin = function(input)
-
-temp = readLines(input)
-df   = data.frame(text = temp,
-                  id   = 1:length(temp))
-
-
-
-
-
 # Membaca data komentar dan melakukan preprocessing sederhana
 setwd("~/RAG-using-R/raw story")
 file  = list.files(pattern = "*txt")
-df    = mclapply(file,readLines,mc.cores = ncore)
-df    = data.table::rbindlist(df,fill = T) |> as.data.frame()
 
+# kita bikin function untuk mengambil datanya
+# input = file[[1]]
+ambilin = function(input){
+  # pengambilan data
+  temp = readLines(input)
+  temp = paste(temp,collapse = "")
+  return(temp)
+}
+# kita ambil semua data yang ada
+df = mclapply(file,ambilin,mc.cores = ncore) |> unlist()
 
-
-
-komen = df |> janitor::clean_names()
-komen = komen |> pull(judul) |> unique() |> sort()
-
-input = data.frame(id = 1:length(komen),text = komen)
-
-pre_data =
-  input %>%
-  unnest_tokens("words",text) %>%
-  filter(!words %in% stop) %>% 
-  filter(stringr::str_length(words) > 3) %>% 
-  group_by(id) %>% 
-  summarise(komen = paste(words,collapse = " ")) %>% 
-  ungroup()
-
-komen = pre_data$komen
-
-# save(komen,file = "to colab.rda")
+# kita akan save menjadi data yang ready
+setwd("~/RAG-using-R")
+komen = df
+save(komen,file = "to colab.rda")
